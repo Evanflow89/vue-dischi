17dbfca957e101c0011b15dd4a466bdb25cc1b3d
Evanflow89 <ivanbaragone@gmail.com>
2022-06-07 17:26:07 +0200

    Added Axios

---
+++ src/components/blocks/BaseMain.vue
@@ -0,0 +1,18 @@
+<template>
+  <main>
+    <SectionAlbums />
+  </main>
+</template>
+
+<script>
+import SectionAlbums from "../sections/SectionAlbums.vue";
+export default {
+  name: "BaseMain",
+  components: {
+    SectionAlbums,
+  },
+};
+</script>
+
+<style lang="scss">
+</style>
---
+++ src/components/sections/SectionAlbums.vue
@@ -0,0 +1,36 @@
+<template>
+  <section>
+    <div class="container">
+      <div class="row"></div>
+    </div>
+  </section>
+</template>
+
+<script>
+import axios from "axios";
+
+export default {
+  name: "SectionAlbums",
+  data() {
+    return {
+      albums: [],
+    };
+  },
+
+  created() {
+    axios
+      .get("https://flynn.boolean.careers/exercises/api/array/music")
+      .then((response) => {
+        // handle success
+        this.albums = response.response.data;
+      })
+      .catch((error) => {
+        // handle error
+        console.log(error);
+      });
+  },
+};
+</script>
+
+<style>
+</style>
