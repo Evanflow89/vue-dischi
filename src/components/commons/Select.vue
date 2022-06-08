0802e0929018b0f33d4a6bf5ddfa9c1c332cb649
Evanflow89 <ivanbaragone@gmail.com>
2022-06-08 19:55:41 +0200

    Select Value printed on console

--- src/components/commons/Select.vue
+++ src/components/commons/Select.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <form class="d-flex" @submit.prevent="$emit('searching', selectValue)">
-      <select name="select-genre" id="genre" v-model="selectValue">
+      <select name="select-genre" id="genre" v-model="DataShared.selectValue">
         <option value="all">All</option>
         <option value="rock">Rock</option>
         <option value="pop">Pop</option>
@@ -13,11 +13,12 @@
 </template>
 
 <script>
+import DataShared from "../../shared/DataShared";
 export default {
   name: "Select-bar",
   data() {
     return {
-      selectValue: "",
+      DataShared,
     };
   },
 };
