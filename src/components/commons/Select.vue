8185e0cb325de81b7d514c29b817ceb82216bce7
Evanflow89 <ivanbaragone@gmail.com>
2022-06-08 17:53:52 +0200

    Started Select Input

---
+++ src/components/commons/Select.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <form class="d-flex" @submit.prevent="$emit('searching', Select)">
+      <select name="select-genre" id="genre">
+        <option value="rock">Rock</option>
+        <option value="pop">Pop</option>
+        <option value="jazz">Jazz</option>
+        <option value="metal">Metal</option>
+      </select>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
